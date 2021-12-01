0d66fcee2417984d25c9eb79a2cd85cbf7741a5d
Yustyn <yustyn23@gmail.com>
2021-12-01 15:04:10 +0200

    1:41:22 My not working varriant CLASSWORK

--- electro/settings.py
+++ electro/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
+    'shop', 'cart'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
